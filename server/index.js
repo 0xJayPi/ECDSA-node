698131f167d52504de31432db9eae9ac5d20b57b
0xJayPi <jpcampaya@gmail.com>
2022-12-17 17:05:26 +0100

    MVP completed

--- server/index.js
+++ server/index.js
@@ -7,9 +7,9 @@ app.use(cors());
 app.use(express.json());
 
 const balances = {
-  "0x1": 100,
-  "0x2": 50,
-  "0x3": 75,
+  "043b8186db5c3988d5bee8e1027923d75ad1829d457816306a06f3f9bfe4d2b8241400f34d6fa59786994571fdcc2b4ddb5a8c5be5db4b68a1f36cf73acebdc2a8": 100,
+  "048b005b1fc492e4921533f04259c5ed28c3beedf43a558b9a7f5b446c1dfdd5fdc9a389fc76b1053e9d6eec260f41d077970ccf8004739ed52982d6bc7f8dd294": 50,
+  "040e21bce49731289e1fa923ecb21a869d05da447efc04e0dd8ae47c13574e9b08da721fe32b23a03ccf95414d36ca557825f10c1e5ca260dfac5515ae8bace2fd": 75,
 };
 
 app.get("/balance/:address", (req, res) => {
@@ -19,6 +19,9 @@ app.get("/balance/:address", (req, res) => {
 });
 
 app.post("/send", (req, res) => {
+  // TODO: get a signature form your client-side app
+  // recover public address from such signature
+
   const { sender, recipient, amount } = req.body;
 
   setInitialBalance(sender);
